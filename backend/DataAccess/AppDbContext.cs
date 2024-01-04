6b3ff73d8525ecf46e156f0800a0200fd66da078
zenci125 <zejnalovnihat2@gmail.com>
2024-01-04 15:31:06 +1000

    Fix DbContext

--- backend/DataAccess/AppDbContext.cs
+++ backend/DataAccess/AppDbContext.cs
@@ -13,36 +13,4 @@ public class AppDbContext : DbContext
     {
         modelBuilder.Entity<User>().HasIndex("Login").IsUnique(true);
     }
-
-    public void InitializeData()
-    {
-        if (!Questions.Any())
-        {
-            var question1 = new Question
-            {
-                Text = "Какая столица Франции?",
-                Options = new List<AnswerOption>
-                {
-                    new AnswerOption { Text = "Париж", Correct = true },
-                    new AnswerOption { Text = "Лондон", Correct = false },
-                    new AnswerOption { Text = "Берлин", Correct = false }
-                }
-            };
-
-            var question2 = new Question
-            {
-                Text = "Сколько планет в солнечной системе?",
-                Options = new List<AnswerOption>
-                {
-                    new AnswerOption { Text = "8", Correct = true },
-                    new AnswerOption { Text = "7", Correct = false },
-                    new AnswerOption { Text = "9", Correct = false }
-                }
-            };
-
-            Questions.AddRange(question1, question2);
-            SaveChanges();
-        }
-    }
-
 }
