3d2d87c16190e5cc78d7537949ec833644a26ee4
zenci125 <zejnalovnihat2@gmail.com>
2024-01-07 22:56:47 +1000

    Add QuestionId in request

--- backend/Controllers/AnswerOptionController.cs
+++ backend/Controllers/AnswerOptionController.cs
@@ -96,6 +96,7 @@ namespace Controllers
 
     public class AnswerOptionRequest
     {
+        public Guid QuestionId { get; set; }
         public required string Text {get; set;}
         public bool Correct {get; set;}
 
