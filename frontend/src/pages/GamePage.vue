ae4289cf1b56eef380667c1cf448c83edafcb339
dolaprolorap <dolaprolorapff@gmail.com>
2024-01-08 20:11:30 +1000

    intermediate changes

---
+++ frontend/src/pages/GamePage.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-page class='row justify-around items-center'>
+    <q-card
+      fit
+      style="width: 80%; height: 600px;"
+    >
+    </q-card>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+  import * as signalR from "@microsoft/signalr";
+
+  const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/Game")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+  async function start() {
+      try {
+          await connection.start();
+          console.log("SignalR Connected.");
+      } catch (err) {
+          console.log(err);
+          setTimeout(start, 5000);
+      }
+  };
+
+  connection.onclose(async () => {
+      await start();
+  });
+
+  // Start the connection.
+  start();
+</script>
