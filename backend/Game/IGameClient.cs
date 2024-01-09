184b75f0a85f083778dff6044eea22398c1b4e1b
krasnov-la <leonid.krasnov.r255@gmail.com>
2024-01-09 22:08:02 +1000

    final(i hope) tweaks

--- backend/Game/IGameClient.cs
+++ backend/Game/IGameClient.cs
@@ -9,9 +9,10 @@ public interface IGameClient
     public Task StartTurnExpand(string login);
     public Task StartTurnDuel(string login);
     public Task Obtain(string login, int areaId);
-    public Task ExpandChoise(string login, int areaId);
+    public Task ExpandChoise(string login, int? areaId);
     public Task ExpandChoisesDrop();
     public Task EndTurn();
     public Task<bool> AskQuestion(Guid questionId);
     public Task WrongOrderMove(string expected, string actual);
+    public Task GameEnd();
 }
