abe945573c0f33a6d2e1a93b2329e699754e38f5
dolaprolorap <dolaprolorapff@gmail.com>
2024-01-09 21:15:56 +1000

    add test question popup + test map parts

---
+++ frontend/src/components/QuestionPopup.vue
@@ -0,0 +1,89 @@
+<template>
+  <q-dialog
+    :model-value=active
+  >
+    <q-card 
+      class="column justify-center"
+      style="width: 700px; height: 400px;"
+    >
+      <q-card-section class="row text-center justify-center items-center row-1">
+        <span class="col-3">
+          <q-avatar 
+            style="height: 50px; width: 50px;"
+          >
+            <img :src="avatarSrcPlayer1"/> 
+          </q-avatar>
+        </span>
+        <span class="col-6">{{ ratingPlayer1 }} / {{ ratingPlayer2 }}</span>
+        <span class="col-3">
+          <q-avatar 
+            style="height: 50px; width: 50px;"
+          >
+            <img :src="avatarSrcPlayer2"/> 
+          </q-avatar>
+        </span>
+      </q-card-section>
+      <q-card-section class="row text-center justify-center row-1">
+        <span>{{ timerCounter }}</span>
+      </q-card-section>
+      <q-card-section class="row text-center justify-center row-2">
+        <span>{{ question }}</span>
+      </q-card-section>
+      <q-card-section class="row text-center justify-center row-6">
+        <div class="column">
+          <div class="row col-5">
+            <q-btn 
+              color="primary q-my-sm q-mr-sm"
+              style="width: 200px;"
+            >
+              {{ ans1 }}
+            </q-btn>
+            <q-btn 
+              color="primary q-my-sm"
+              style="width: 200px;"
+            >
+              {{ ans3 }}
+            </q-btn>
+          </div>
+          <div class="row col-5">
+            <q-btn 
+              color="primary q-my-sm q-mr-sm"
+              style="width: 200px;"
+            >
+              {{ ans2 }}
+            </q-btn>
+            <q-btn 
+              color="primary q-my-sm"
+              style="width: 200px;"
+            >
+              {{ ans4 }}
+            </q-btn>
+          </div>
+        </div>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup lang="ts">
+  import { ref } from "vue";
+
+  const props = defineProps<{
+    active: boolean,
+    avatarSrcPlayer1: string,
+    avatarSrcPlayer2: string,
+    ratingPlayer1: number,
+    ratingPlayer2: number,
+    question: string,
+    ans1: string,
+    ans2: string,
+    ans3: string,
+    ans4: string,
+  }>();
+
+  const emit = defineEmits<{
+    (e: "answer", ansId: number): void
+  }>();
+
+  const timerCounter = ref<number>(5);
+</script>
--- frontend/src/pages/GamePage.vue
+++ frontend/src/pages/GamePage.vue
@@ -3,18 +3,72 @@
     <q-card @click="send"
       fit
       style="width: 80%; height: 600px;"
+      class="column"
     >
+      <q-table
+        flat bordered
+        grid
+        title="KARTA DVFU"
+        :rows="rows"
+        :columns="columns"
+        row-key="name"
+        hide-header
+      >
+        <template v-slot:item="props">
+          <div
+            class="q-pa-xs col-xs-12 col-sm-6 col-md-4 col-lg-3 grid-style-transition"
+          >
+            <q-card bordered flat @click="() => { console.log(123) }">
+              <q-list dense>
+                <q-item v-for="col in props.cols.filter(col => col.name !== 'desc')" :key="col.name">
+                  <q-item-section>
+                    <q-item-label>{{ col.label }}</q-item-label>
+                  </q-item-section>
+                  <q-item-section side>
+                    <q-item-label caption>{{ col.value }}</q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-card>
+          </div>
+        </template>
+      </q-table>
     </q-card>
+    <QuestionPopup
+        v-model:active=active
+        :avatarSrcPlayer1=avatarSrcPlayer1
+        :avatarSrcPlayer2=avatarSrcPlayer2
+        :ratingPlayer1=ratingPlayer1
+        :ratingPlayer2=ratingPlayer2
+        :question=question
+        :ans1=ans1
+        :ans2=ans2
+        :ans3=ans3
+        :ans4=ans4
+    />
   </q-page>
 </template>
 
 <script setup lang="ts">
   import * as signalR from "@microsoft/signalr";
+  import QuestionPopup from "src/components/QuestionPopup.vue";
+  import { ref } from "vue";
 
   const connection = new signalR.HubConnectionBuilder()
     .withUrl("http://localhost:5144/Game", {accessTokenFactory: () => "real access token goes here (maybe refresh before returning)"})
     .configureLogging(signalR.LogLevel.Information)
     .build();
+
+  const active = ref(false);
+  const avatarSrcPlayer1 = ref("https://cdn.quasar.dev/img/avatar2.jpg");
+  const avatarSrcPlayer2 = ref("https://cdn.quasar.dev/img/avatar2.jpg");
+  const ratingPlayer1 = ref(123);
+  const ratingPlayer2 = ref(123);
+  const question = ref("123");
+  const ans1 = ref("123");
+  const ans2 = ref("123");
+  const ans3 = ref("123");
+  const ans4 = ref("123");
 
   async function start() {
       try {
@@ -39,4 +93,20 @@
   {
     connection.send("Echo", "message");
   }
+
+  const columns = [
+    {
+      name: 'number', 
+      label: 'Number', 
+      field: 'number'
+    }
+  ]
+
+  const mapPartsCount = 20;
+
+  const rows = Array.from(Array(mapPartsCount).keys()).map(val => ({
+    number: val
+  }));
+
+  console.log(rows);
 </script>
